5188ac4550537b3e683491a974d23d7f51076573
BulatovicBalsa <baki.b.2k02@gmail.com>
2025-11-09 22:55:17 +0100

    initialize Spring Boot project with Gradle setup and basic configuration

---
+++ src/main/kotlin/com/example/plugin_variants_resolver/PluginVariantsResolverApplication.kt
@@ -0,0 +1,11 @@
+package com.example.plugin_variants_resolver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PluginVariantsResolverApplication
+
+fun main(args: Array<String>) {
+	runApplication<PluginVariantsResolverApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/plugin_variants_resolver/PluginVariantsResolverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.plugin_variants_resolver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PluginVariantsResolverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
